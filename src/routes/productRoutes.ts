46fb6f8c66b516df354007c85c91deed2c831fd0
ramasul <ramasulaimannurcahyo@mail.ugm.ac.id>
2024-11-26 21:47:30 +0700

    fix: Authorization

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,18 +1,18 @@
-import { Router } from 'express';
-import ProductController from '../controllers/productController';
-import { authMiddleware } from '../middleware/auth';
+import { Router } from "express";
+import ProductController from "../controllers/productController";
+import { authMiddleware } from "../middleware/auth";
 
 const router = Router();
 
 // Public routes
-router.get('/', ProductController.getAllProducts);
-router.get('/store/:store_id', ProductController.getProductsByStore);
-router.get('/:id', ProductController.getProductById);
+router.get("/", ProductController.getAllProducts);
+router.get("/store/:store_id", ProductController.getProductsByStore);
+router.get("/:id", ProductController.getProductById);
 
 // Protected routes
-router.use(authMiddleware);
-router.post('/', ProductController.createProduct);
-router.put('/:id', ProductController.updateProduct);
-router.delete('/:id', ProductController.deleteProduct);
+// router.use(authMiddleware);
+router.post("/", ProductController.createProduct);
+router.put("/:id", ProductController.updateProduct);
+router.delete("/:id", ProductController.deleteProduct);
 
 export default router;
