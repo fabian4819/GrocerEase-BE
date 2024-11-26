ddb02f1a7753cc780cedaae0d4dfb1916028972d
ramasul <ramasulaimannurcahyo@mail.ugm.ac.id>
2024-11-26 17:06:34 +0700

    fix: Product Models

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,42 +1,39 @@
-import mongoose, { Schema, Document } from 'mongoose';
+import mongoose, { Schema, Document } from "mongoose";
 
 export interface IProduct extends Document {
-    product_id: string;
-    name: string;
-    description: string;
-    price: number;
-    image_link: string;
-    store_id: string;
+  name: string;
+  description: string;
+  price: number;
+  image_link: string;
+  store_id: mongoose.Schema.Types.ObjectId;
 }
 
-const ProductSchema = new Schema({
-    product_id: {
-        type: String,
-        required: true,
-        unique: true
-    },
+const ProductSchema = new Schema(
+  {
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     description: {
-        type: String,
-        default: "-"
+      type: String,
+      default: "-",
     },
     price: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     image_link: {
-        type: String,
-        default: '/img/default-product.jpg' 
+      type: String,
+      default: "/img/default-product.jpg",
     },
     store_id: {
-        type: String,
-        required: true,
-        ref: 'Store'
-    }
-}, { timestamps: true });
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "Store",
+    },
+  },
+  { timestamps: true }
+);
 
-const Product = mongoose.model<IProduct>('Product', ProductSchema);
+const Product = mongoose.model<IProduct>("Product", ProductSchema);
 export default Product;
